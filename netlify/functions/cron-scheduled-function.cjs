e5dfb6cdfba8759a89c6822b7d30427c7602b827
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2024-04-24 13:12:21 +0300

    fixed url

--- netlify/functions/cron-scheduled-function.cjs
+++ netlify/functions/cron-scheduled-function.cjs
@@ -3,9 +3,12 @@ require('../../index');
 const handler = async () => {
   console.log('invoke');
   try {
-    await fetch('/.netlify/functions/cron', {
-      method: 'POST',
-    });
+    await fetch(
+      'https://mellow-basbousa-4a0130.netlify.app/.netlify/functions/cron',
+      {
+        method: 'POST',
+      },
+    );
   } catch (error) {
     console.log(error);
   }
