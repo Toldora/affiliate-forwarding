a5a5c8ea53464c865583bbeee8c22fcd6b9e71c0
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2024-07-25 13:50:02 +0300

    added full logs

--- controllers/alanbase.js
+++ controllers/alanbase.js
@@ -1,12 +1,11 @@
 const { alanbaseApi } = require('../api');
 require('dotenv').config();
 
-const buildErrorInfo = (errorData, initialData) => {
-  return Object.keys(errorData.errors).reduce((acc, key) => {
+const buildErrorInfo = (errorData, initialData) =>
+  Object.keys(errorData.errors).reduce((acc, key) => {
     const index = key.split('.')[0];
     return { ...acc, [index]: initialData[index] };
   }, {});
-};
 
 const postGoals = async goals => {
   try {
@@ -19,7 +18,7 @@ const postGoals = async goals => {
       goals,
     );
   } catch (error) {
-    const data = error.response.data;
+    const { data } = error.response;
     console.log('postGoals error', data);
     const goalsWithErrors = buildErrorInfo(data, goals);
     console.log('Goals with errors', goalsWithErrors);
@@ -32,18 +31,19 @@ const postEvents = async events => {
   try {
     console.log(
       'postEvents',
-      events.map(({ click_id, datetime, event }) => ({
-        click_id,
-        datetime,
-        event,
-      })),
+      events,
+      // events.map(({ click_id, datetime, event }) => ({
+      //   click_id,
+      //   datetime,
+      //   event,
+      // })),
     );
     await alanbaseApi.post(
       `/events/${process.env.ALANBASE_API_PATH_KEY}`,
       events,
     );
   } catch (error) {
-    const data = error.response.data;
+    const { data } = error.response;
     console.log('postEvents error', data);
     const eventsWithErrors = buildErrorInfo(data, events);
     console.log('Events with errors', eventsWithErrors);
